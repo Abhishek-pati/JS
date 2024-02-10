51d4539f21059f06a9ca4ac2d4c6f0a8c01e4738
Abhishek Pati <153745994+Abhishek-pati@users.noreply.github.com>
2024-02-10 06:37:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"//treat all js code as newer version
+
+//alert(5+5)// we are using nodejs ,not browser
+
+console.log(3+3);
+console.log("Abhishek");
+
+/*
+int,bigint 
+string
+boolean
+null
+*/
+
+console.log(typeof null);
