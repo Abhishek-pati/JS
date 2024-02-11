485bcab8084eb0366d06871131d0142b282d7dfa
Abhishek Pati <153745994+Abhishek-pati@users.noreply.github.com>
2024-02-11 06:30:25 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,23 @@
+let s1 = 33
+console.log(typeof s1);
+
+let s2 = "abc"
+let valueInnumber= Number(s2)
+console.log(valueInnumber);
+
+let s3 = ""
+let boolean = Boolean(s3)
+console.log(boolean);
+
+let s4 = 45
+let valueInstring = String(s4)
+console.log(valueInstring);
+
+/*
+"33"=>Number
+"12abc"=>NaN(not a number)
+
+"true"=>1 , "false=>0"
+
+""=>false , "Abhishek"=>true
+ */
