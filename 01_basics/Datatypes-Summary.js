6493c49ee77822b86a86014f9dea81b70630af64
Abhishek Pati <153745994+Abhishek-pati@users.noreply.github.com>
2024-02-13 06:46:41 +0000

    Stack and Heap Memory in JS

--- 01_basics/Datatypes-Summary.js
+++ 01_basics/Datatypes-Summary.js
@@ -12,6 +12,8 @@ const id = Symbol('458')
 const newId = Symbol('458')
 
 console.log(id == newId);
+
+
 //Object
 let myObj = {
     name:"Abhishek",
@@ -23,3 +25,26 @@ let myObj = {
 const myFunction = function(){
     console.log("Hello World");
 }
+
+// stack(primitive) heap(non-Primitive)
+
+let myNmae = "Abhishek pati"
+
+let myOthername = myNmae
+
+myOthername = "Abhishek"
+
+console.log(myOthername);
+console.log(myNmae);
+
+let userOne = {
+    email :"xyz@gmail.com",
+    upi : "123@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "abc@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
