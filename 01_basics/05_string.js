579e3fad7d6165934876148f85a5e01a8db4d057
Abhishek Pati <153745994+Abhishek-pati@users.noreply.github.com>
2024-02-14 17:49:47 +0000

    Stringss

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const myName = "Abhishek"
+const myRepo = 50
+
+console.log(`My name is : ${myName} and my total no of repo is ${myRepo}`);
+
+const gameName = new String("BGMI")
+
+console.log(gameName.charAt(2));
