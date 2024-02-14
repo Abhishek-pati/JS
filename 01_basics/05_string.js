15607594242e96408b513dd03b2f4cd8d6160f4c
Abhishek Pati <153745994+Abhishek-pati@users.noreply.github.com>
2024-02-14 18:13:17 +0000

    Strings

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,8 +1,14 @@
 const myName = "Abhishek"
 const myRepo = 50
-
 console.log(`My name is : ${myName} and my total no of repo is ${myRepo}`);
 
 const gameName = new String("BGMI")
-
 console.log(gameName.charAt(2));
+
+const stringOne = "RangeRover"
+console.log(stringOne.substring(0,4));
+console.log(stringOne.length);
+console.log(stringOne.slice(-10,2));
+
+const stringTwo = "    BMW    "
+console.log(stringTwo.trim());
