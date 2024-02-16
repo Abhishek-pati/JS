bd03cf63b92e71fb9c3cf9369ab23514f219d172
Abhishek Pati <153745994+Abhishek-pati@users.noreply.github.com>
2024-02-16 15:25:33 +0000

    Nums and Maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,23 @@
+//*************************** Nums ****************************
+const score = 500
+const balance = new Number(100)
+console.log(score);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 21.99
+console.log(otherNumber.toPrecision(2));
+
+const anotherNumber = 1000000
+console.log(anotherNumber.toLocaleString());
+console.log(anotherNumber.toLocaleString('en-IN'));
+
+//*************************** Maths ****************************
+
+console.log(Math.abs(-5));
+console.log(Math.round(45.55));
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 5
+const max = 20
+console.log(Math.floor(Math.random()*max-min)+min);
