50d8a102b112c286c86f52c7aa009322f0f7cf07
Abhishek Pati <153745994+Abhishek-pati@users.noreply.github.com>
2024-02-26 13:17:44 +0000

    scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,12 @@
+// var c = 100
+let a = 200
+if(true){
+    let a = 20
+    const b = 20
+    var c = 54
+    console.log("INNER : ",a);
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
