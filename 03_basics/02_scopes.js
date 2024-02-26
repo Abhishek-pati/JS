e93d10e5b732669590d4bd43325528c27606647b
Abhishek Pati <153745994+Abhishek-pati@users.noreply.github.com>
2024-02-26 13:38:21 +0000

    Scopes in JS

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,45 @@ if(true){
     let a = 20
     const b = 20
     var c = 54
-    console.log("INNER : ",a);
+    // console.log("INNER : ",a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one (){
+    const userName = "Abhishek"
+    function two (){
+        const website = "Youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+if(true){
+    const userName = "Rohan"
+    if(userName === "Rohan"){
+        const website = " google"
+        console.log(userName+website);
+    }
+    // console.log(website);
+}
+// console.log(userName);
+
+// +++++++++++++++ Insteresting +++++++++++++++++
+
+
+console.log(addOne(5));
+function addOne(num){
+    return num+1
+}
+
+console.log(addTwo(5));
+const addTwo= function (num){
+    return num+2
+}
+
