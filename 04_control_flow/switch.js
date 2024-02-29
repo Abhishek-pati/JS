1071e67330dc61dd5128a4d4a9a7db59f7dbb816
Abhishek Pati <153745994+Abhishek-pati@users.noreply.github.com>
2024-02-29 14:09:36 +0000

    Control FLow Code Files

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,18 @@
+const month = 5
+
+switch (month) {
+    case 1 :
+    console.log("Jan");
+        
+        break;
+    case 2 :
+        console.log("Feb");
+        break;
+    case 3 :
+        console.log("Mar");
+        break;
+
+    default:
+        console.log("Default Value");
+        break;
+}
